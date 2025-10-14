01616783e96bf3e6a67d0dd57f25e62144eb5c17
Marco <maidh91@gmail.com>
2025-10-14 12:57:11 +1100

    v1 compat

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const process = require('process')
 const main = require('./src/main')
 
 let args = []
-if (isPear) args = global.Pear.config.args
-else if (isBare) args = global.Bare.argv.slice(2)
+if (isPear) args = (Pear.app || Pear.config).args
+else if (isBare) args = Bare.argv.slice(2)
 else args = process.argv.slice(2)
 
 main(args)
